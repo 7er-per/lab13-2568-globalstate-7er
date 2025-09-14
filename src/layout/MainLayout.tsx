3d7ab97d92e465114a63768db482ed140a8050f6
7er-per <wasawat.git@gmail.com>
2025-09-14 23:43:20 +0700

    doneV2

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -27,8 +27,8 @@ export default function MainLayout() {
       <AppShell.Footer>
         <FooterComponent
           year="2025"
-          fullName="chanadda"
-          studentId="670612039"
+          fullName="Wasawat"
+          studentId="670610731"
         />
       </AppShell.Footer>
 
